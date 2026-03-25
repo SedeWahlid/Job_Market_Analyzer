d3e4d2161e444bb67fdd01c3721779a4711ed4cb
Wahlid Sedeghi <Sede.wahlid@gmail.com>
2026-03-25 11:13:55 +0100

    Massive change in data logic : - fixed issue with tech stack extraction , by using regex

--- backend.py
+++ backend.py
@@ -3,6 +3,7 @@ import pandas as pd
 from supabase import create_client
 from os import getenv
 from numpy import nan
+import re
 
  
 #################################################
@@ -19,16 +20,22 @@ def clean_job_data(jobs):
     return cleaned_jobs
 
 def extract_tech_stack(description, tech_keywords):
-    # Convert description to lowercase for case-insensitive matching
+    if not isinstance(description, str):
+        return []
     try:
         description_lower = description.lower()
+        tech_stack = []
         
-        # Find all tech stack items present in the description
-        tech_stack = [keyword for keyword in tech_keywords if keyword.lower() in description_lower]
+        for keyword in tech_keywords:
+            pattern = r'(?<![\w\-])' + re.escape(keyword.lower()) + r'(?![\w\-\+#])'
+            if re.search(pattern, description_lower):
+                tech_stack.append(keyword)
+                
+        return tech_stack
+        
     except Exception as e:
-        print(f"Error occured while extracting Tech stacks from Descriptions with error: {e}\n")
+        print(f"Error occurred while extracting Tech stacks from Descriptions with error: {e}\n")
         return None,[]
-    return tech_stack
 
 def insert_data_to_supabase(cleaned_jobs):
     try:
@@ -60,30 +67,93 @@ def insert_data_to_supabase(cleaned_jobs):
 
 # list of technology keywords to look for in job descriptions
 tech_keywords = [
-    'python', 'java', 'c++', 'javascript', 'sql', 'aws', 'docker', 'kubernetes', 
-    'react', 'angular', 'ruby', 'go', 'swift', 'kotlin', 'typescript', 'scala', 
-    'perl', 'php', 'rust', 'haskell', 'elixir', 'clojure', 'groovy', 'objective-c', 
-    'vue.js', 'ember.js', 'svelte', 'backbone.js', 'polymer', 'flutter', 'react-native', 
-    'ionic', 'cordova', 'node.js', 'express', 'django', 'flask', 'rails', 'sinatra', 
-    'laravel', 'symfony', 'asp.net', 'spring', 'hibernate', 'jpa', 'mybatis', 'sequelize', 
-    'mongoose', 'typeorm', 'mongodb', 'mysql', 'postgresql', 'oracle', 'sqlite', 'redis', 
-    'memcached', 'cassandra', 'elasticsearch', 'neo4j', 'apache', 'nginx', 'haproxy', 
-    'traefik', 'jenkins', 'gitlab-ci', 'github-actions', 'circleci', 'travis-ci', 'bitbucket-pipelines', 
-    'kibana', 'grafana', 'prometheus', 'graylog', 'splunk', 'logstash', 'ansible', 
-    'terraform', 'puppet', 'chef', 'saltstack', 'docker-compose', 'k8s', 'eks', 
-    'gke', 'aks', 'istio', 'linkerd', 'consul', 'vault', 'opa', 'csp', 'mfa', 
-    'oauth2', 'jwt', 'openapi', 'graphql', 'grpc', 'protobuf', 'restful', 'microservices', 
-    'serverless', 'lambda', 'azure-functions', 'google-cloud-functions', 'firebase-functions', 
-    'gcp', 'aws-iam', 'azure-ad', 'okta', 'auth0', 'keycloak', 'openid-connect', 
-    'saml', 'jwt-auth', 'oauth2-auth', 'api-gateway', 'nginx-ingress', 'haproxy-load-balancer', 
-    'traefik-router', 'kong-api-gateway', 'aws-waf', 'azure-waf', 'google-cloud-waf', 
-    'gcp-firewall', 'aws-s3', 'azure-blob-storage', 'google-cloud-storage', 'aws-sqs', 
-    'azure-queue-storage', 'google-cloud-pubsub', 'aws-dynamodb', 'azure-cosmosdb', 
-    'google-cloud-datastore', 'aws-rds', 'azure-sql', 'google-cloud-sql', 'aws-elb', 
-    'azure-lb', 'google-cloud-ilb', 'aws-ecs', 'azure-acr', 'google-cloud-container-builder', 
-    'aws-codebuild', 'azure-devops', 'google-cloud-build', 'aws-vpc', 'azure-vnet', 
-    'google-cloud-vpc', 'aws-eks', 'azure-aks', 'google-cloud-gke', 'aws-lambda', 
-    'azure-functions', 'google-cloud-functions'
+    # --- Programming Languages ---
+    'python', 'java', 'c', 'c++', 'c#', 'javascript', 'typescript', 'sql', 'ruby', 
+    'go', 'golang', 'swift', 'kotlin', 'scala', 'perl', 'php', 'rust', 'haskell', 
+    'elixir', 'clojure', 'groovy', 'objective-c', 'f#', 'r', 'matlab', 'dart', 
+    'lua', 'julia', 'shell', 'bash', 'powershell', 'vba', 'assembly', 'cobol', 
+    'fortran', 'solidity', 'erlang',
+
+    # --- Frontend & UI ---
+    'html', 'css', 'sass', 'less', 'tailwind', 'bootstrap', 'material-ui', 
+    'react', 'react.js', 'angular', 'vue', 'vue.js', 'ember.js', 'svelte', 
+    'backbone.js', 'polymer', 'jquery', 'next.js', 'nuxt.js', 'gatsby', 
+    'webpack', 'vite', 'babel', 'redux', 'mobx', 'rxjs', 'd3.js', 'three.js', 
+    'webgl', 'webassembly', 'wasm',
+
+    # --- Backend & Frameworks ---
+    'node.js', 'node', 'express', 'django', 'flask', 'fastapi', 'tornado', 
+    'rails', 'ruby on rails', 'sinatra', 'laravel', 'symfony', 'asp.net', 
+    '.net', '.net core', 'spring', 'spring boot', 'hibernate', 'jpa', 'mybatis', 
+    'sequelize', 'mongoose', 'typeorm', 'phoenix', 'play', 'ktor', 'nestjs', 
+    'meteor', 'strapi', 'celery',
+
+    # --- Mobile Development ---
+    'flutter', 'react-native', 'ionic', 'cordova', 'android', 'ios', 
+    'xamarin', 'maui', 'appium',
+
+    # --- Databases & Data Warehouses ---
+    'mongodb', 'mysql', 'postgresql', 'postgres', 'oracle', 'sqlite', 'redis', 
+    'memcached', 'cassandra', 'elasticsearch', 'neo4j', 'mariadb', 'sql server', 
+    'mssql', 'db2', 'couchbase', 'couchdb', 'dynamodb', 'cosmosdb', 'firebase', 
+    'supabase', 'snowflake', 'redshift', 'bigquery', 'databricks', 'teradata', 
+    'presto', 'athena', 'hbase', 'influxdb', 'timescaledb', 'realm',
+
+    # --- Message Brokers & Streaming ---
+    'kafka', 'rabbitmq', 'activemq', 'zeromq', 'pulsar', 'kinesis', 'spark', 
+    'flink', 'hadoop', 'hive', 'storm',
+
+    # --- AI, Machine Learning & Data Science ---
+    'pandas', 'numpy', 'scipy', 'scikit-learn', 'tensorflow', 'pytorch', 
+    'keras', 'opencv', 'nltk', 'spacy', 'hugging face', 'langchain', 'jupyter', 
+    'matplotlib', 'seaborn', 'llm', 'openai',
+
+    # --- DevOps, CI/CD & Infrastructure as Code ---
+    'jenkins', 'gitlab-ci', 'github-actions', 'circleci', 'travis-ci', 
+    'bitbucket-pipelines', 'ansible', 'terraform', 'puppet', 'chef', 'saltstack', 
+    'argocd', 'flux', 'sonarcloud', 'sonarqube',
+
+    # --- Containers & Orchestration ---
+    'docker', 'docker-compose', 'kubernetes', 'k8s', 'eks', 'gke', 'aks', 
+    'openshift', 'helm',
+
+    # --- Cloud Platforms & Services ---
+    'aws', 'gcp', 'azure', 'heroku', 'digitalocean', 'linode', 'vercel', 
+    'netlify', 'cloudflare', 'fastly', 'aws-s3', 'azure-blob-storage', 
+    'google-cloud-storage', 'aws-sqs', 'azure-queue-storage', 'google-cloud-pubsub', 
+    'aws-dynamodb', 'azure-cosmosdb', 'google-cloud-datastore', 'aws-rds', 
+    'azure-sql', 'google-cloud-sql', 'aws-elb', 'azure-lb', 'google-cloud-ilb', 
+    'aws-ecs', 'azure-acr', 'google-cloud-container-builder', 'aws-codebuild', 
+    'azure-devops', 'google-cloud-build', 'aws-vpc', 'azure-vnet', 'google-cloud-vpc', 
+    'aws-eks', 'azure-aks', 'google-cloud-gke', 'aws-lambda', 'azure-functions', 
+    'google-cloud-functions', 'firebase-functions',
+
+    # --- Servers, Proxies & API Gateways ---
+    'apache', 'nginx', 'haproxy', 'traefik', 'tomcat', 'api-gateway', 
+    'nginx-ingress', 'haproxy-load-balancer', 'traefik-router', 'kong-api-gateway',
+
+    # --- Monitoring, Logging & Observability ---
+    'kibana', 'grafana', 'prometheus', 'graylog', 'splunk', 'logstash', 
+    'datadog', 'new relic', 'appdynamics', 'dynatrace', 'sentry', 'elk',
+
+    # --- Security, Identity & Auth ---
+    'istio', 'linkerd', 'consul', 'vault', 'opa', 'csp', 'mfa', 'oauth2', 
+    'jwt', 'openapi', 'graphql', 'grpc', 'protobuf', 'restful', 'aws-iam', 
+    'azure-ad', 'okta', 'auth0', 'keycloak', 'openid-connect', 'saml', 
+    'jwt-auth', 'oauth2-auth', 'aws-waf', 'azure-waf', 'google-cloud-waf', 
+    'gcp-firewall',
+
+    # --- Architecture & Concepts (Often listed as requirements) ---
+    'microservices', 'serverless', 'lambda', 'rest', 'soap', 'tdd', 'bdd', 
+    'agile', 'scrum', 'kanban',
+
+    # --- Testing ---
+    'selenium', 'cypress', 'puppeteer', 'playwright', 'jest', 'mocha', 
+    'jasmine', 'karma', 'pytest', 'junit', 'nunit', 'testng', 'postman', 'soapui',
+
+    # --- Version Control & OS ---
+    'git', 'github', 'gitlab', 'bitbucket', 'svn', 'mercurial', 'linux', 
+    'unix', 'windows', 'macos', 'ubuntu', 'centos', 'debian', 'redhat', 'alpine'
 ]
 try:
     jobs = scrape_jobs(
